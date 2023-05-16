3d0329e6520a02e20b177304cf8e7c02c0f0dbf3
SlowentY <vinokurov1308@mail.ru>
2023-05-17 00:08:41 +0300

    Import Update

--- ru/itmo/physics/Engine3D/Main.java
+++ ru/itmo/physics/Engine3D/Main.java
@@ -1,4 +1,21 @@
 package ru.itmo.physics.Engine3D;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Main {
+    public static void main(String[] args)
+    {
+        // Nothing...
+    }
 }
